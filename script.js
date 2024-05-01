626c4c115b53f0d0cd720a96bc088358e172a98d
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-01 20:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function volume_sphere() {
-    //Write your code here
+function volume_sphere(e) {
+  //Write your code here
+  e.preventDefault();
+let radius=document.getElementById("radius");
+let output=document.getElementById("volume");
+if(parseFloat(radius.value)<0){
+output.value=NaN;
+}
+else if(isNaN(radius.value)){
+  output.value=NaN;
   
+}
+else{
+  let r=parseFloat(radius.value);
+  let volume=((4/3)*Math.PI*r*r*r).toFixed(4);
+  output.value=volume;
+
+}
+//console.log(output);
 } 
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
